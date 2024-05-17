837a8fc23d9b5324c95c52763574826fed680738
coder-eb <ebranbright@gmail.com>
2024-05-17 09:49:09 +0530

    LL: find_kth_from_end

--- main.py
+++ main.py
@@ -3,13 +3,26 @@ from DS.Stack import Stack
 from DS.Queue import Queue
 from DS.DoublyLinkedList import DoublyLinkedList
 
+def find_kth_from_end(ll: LinkedList, k):
+    slow = fast = ll.head
+    for iter in range(k):
+        if fast == None:
+            return None
+        fast = fast.next
+    
+    while fast is not None:
+        slow = slow.next
+        fast = fast.next
+
+    return slow
+
 def main():
-    ll = LinkedList(0)
-    # ll.append(1)
-    # ll.append(2)
-    # ll.append(3)
-    # ll.append(4)
-    print(ll.find_middle_node())
+    ll = LinkedList(5)
+    ll.append(4)
+    ll.append(3)
+    ll.append(2)
+    ll.append(1)
+    print(find_kth_from_end(ll, 6))
 
 if __name__ == "__main__":
     main()
