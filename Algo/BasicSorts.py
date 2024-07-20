64faacbc7ee0721ff6481f91d40cf0beaaff15b5
coder-eb <ebranbright@gmail.com>
2024-07-20 09:20:18 +0530

    BasicSorts: Selection Sort

--- Algo/BasicSorts.py
+++ Algo/BasicSorts.py
@@ -5,8 +5,22 @@ def bubble_sort(numbers):
                 numbers[index], numbers[index+1] = numbers[index+1], numbers[index]
     return numbers
 
+def selection_sort(numbers):
+    for index_to_replace in range(len(numbers)-1):
+        min_index = index_to_replace
+        for curr_index in range(index_to_replace+1, len(numbers)):
+            if numbers[curr_index] < numbers[min_index]:
+                min_index = curr_index
+        
+        if min_index == index_to_replace:
+            continue
+        numbers[index_to_replace], numbers[min_index] = numbers[min_index], numbers[index_to_replace]
+        
+    return numbers
+
+
 def main():
-    sorted_list = bubble_sort([4, 3, 2, 1, 6, 5, 12, 9, 8, 7])
+    sorted_list = selection_sort([4, 2, 6, 5, 1, 3]) 
     print(sorted_list)
     
 if __name__ == "__main__":
