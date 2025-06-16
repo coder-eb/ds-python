339cef4ba780ec2c9fe98ba90d2468a6e29ad675
coder-eb <ebranbright@gmail.com>
2025-06-16 23:00:24 +0530

    feat: maxSlidingWindow - deque solution

--- problems/practice.py
+++ problems/practice.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import deque
 from typing import List
 import sys
 import os
@@ -597,22 +597,25 @@ class Problems:
         return matches == 26
     
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        res=[]
-        queue = MonotonicQueue()
-
-        for i in range(0, k):
-            queue.push(nums[i])
-        res.append(queue.max())
+        deq = deque()
+        res = []
 
         l=0
-        for r in range(k, len(nums)):
-            queue.push(nums[r])
-            queue.pop(nums[l])
-            res.append(queue.max())
-            
-            l+=1            
-        return res
+        for r in range(0, len(nums)):
+            while len(deq) > 0 and deq[-1] < nums[r]:
+                deq.pop()
+            deq.append(nums[r])
 
+            if (r-l+1) > k:
+                if deq[0] == nums[l]:
+                    deq.popleft()
+                l+=1
+
+            if r >= k-1:
+                res.append(deq[0])
+            
+        return res
+        
 if __name__ == "__main__":
     problems = Problems()  
-    print(problems.maxSlidingWindow([1,2,1,0,4,2,6],3))
+    print(problems.maxSlidingWindow([1,3,1,2,0,5],3))
