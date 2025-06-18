bd77e60b9d2f6e2ff2c54cab421fc789b0806783
coder-eb <ebranbright@gmail.com>
2025-06-18 08:45:44 +0530

    feat: Trie - self implementation

--- problems/CustomDS.py
+++ problems/CustomDS.py
@@ -19,3 +19,60 @@ class MonotonicQueue:
         
         if self.maxq[0] == n:
             self.maxq.pop(0)
+
+class TrieNode:
+    def __init__(self):
+        self.children = [None]*26
+        self.terminal = False
+
+class Trie:
+    def __init__(self) -> None:
+        self.root = TrieNode()
+    
+    def insert(self, word: str) -> None:
+        curr_node = self.root
+        for i, letter in enumerate(word):
+            terminal = (i == len(word)-1)
+            position = ord(letter)-ord('a')
+            if not curr_node.children[position]:
+                curr_node.children[position] = TrieNode()
+            
+            curr_node = curr_node.children[position]
+            if terminal:
+                curr_node.terminal = terminal
+
+    def search(self, word: str) -> bool:
+        curr_node = self.root
+        for i, letter in enumerate(word):
+            terminal = (i == len(word)-1)
+            position = ord(letter)-ord('a')
+            if not curr_node.children[position]:
+                return False
+            
+            curr_node = curr_node.children[position]
+            if terminal and curr_node.terminal:
+                return True 
+        return False
+
+    def startsWith(self, prefix: str) -> bool:
+        curr_node = self.root
+        for i, letter in enumerate(prefix):
+            position = ord(letter)-ord('a')
+            if not curr_node.children[position]:
+                return False
+            
+            curr_node = curr_node.children[position]
+        return True
+     
+def test_trie():
+    trie = Trie()
+    trie.insert('apple')
+    print(trie.search('applee'))
+    print(trie.startsWith('apple'))
+
+def main():
+    test_trie()
+
+if __name__ == '__main__':
+    main()
+
