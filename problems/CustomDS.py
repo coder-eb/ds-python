22dece402bb8f01636cb1e1930fb220157977f24
coder-eb <ebranbright@gmail.com>
2025-06-19 22:36:31 +0530

    feat: MinStack

--- problems/CustomDS.py
+++ problems/CustomDS.py
@@ -58,7 +58,30 @@ class Trie:
             
             curr_node = curr_node.children[position]
         return True
-     
+
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+        if len(self.min_stack) > 0:
+            val = min(val, self.min_stack[-1])
+        self.min_stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
 def test_trie():
     trie = Trie()
     trie.insert('apple')
@@ -70,4 +93,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
