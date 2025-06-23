2049d91308f56da02a8c5f15c359d6d7b2b674cf
coder-eb <ebranbright@gmail.com>
2025-06-23 23:07:19 +0530

    feat[self]: asteroidCollision

--- problems/practice.py
+++ problems/practice.py
@@ -742,6 +742,23 @@ class Problems:
                 stack.append(i)
         return res
 
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for asteroid in asteroids:
+            stack.append(asteroid)
+            while len(stack) > 1 and stack[-1] < 0 and stack[-2] > 0:
+                reverse = stack.pop()
+                if abs(reverse) == stack[-1]:
+                    stack.pop()
+                elif abs(reverse) > stack[-1]:
+                    stack[-1] = reverse
+
+        return stack
+                    
+
+
+                
+
 if __name__ == "__main__":
     problems = Problems()  
-    print(problems.nextGreaterElements([4,5,2,3]))
+    print(problems.asteroidCollision([5,10,-5]))
