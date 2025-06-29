1a1cc7c34cd7c05b8c7fc2f3875e5edb5bdbf695
coder-eb <ebranbright@gmail.com>
2025-06-29 23:01:50 +0530

    feat[self]: merge sorted array

--- problems/practice.py
+++ problems/practice.py
@@ -890,8 +890,25 @@ class Problems:
         return l
     
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        pass
+        l, r, i = m-1, n-1, m+n-1
+        while l >= 0 and r >= 0:
+            if nums1[l] > nums2[r]:
+                nums1[i] = nums1[l]
+                l-=1
+            else:
+                nums1[i] = nums2[r]
+                r-=1
+            i-=1
+        
+        while r >= 0:
+            nums1[i] = nums2[r]
+            i-=1
+            r-=1
+        
+        return nums1
+
+
 
 if __name__ == "__main__":
     problems = Problems()
-    print(problems.removeElement([0,1,2,2,3,0,4,2],2))
+    print(problems.merge([0],0,[1],1))
